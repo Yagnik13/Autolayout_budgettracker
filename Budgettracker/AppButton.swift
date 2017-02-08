8e1bad3b88442b13b9fd18f1dbe85e298378071f
Yagnik Suthar <14mca54@nirmauni.ac.in>
2017-02-08 12:53:17 +0530

    Initial Commit

---
+++ Budgettracker/AppButton.swift
@@ -0,0 +1,21 @@
+//
+//  appview.swift
+//  Budgettracker
+//
+//  Created by Yagnik on 08/02/17.
+//  Copyright © 2017 Yagnik. All rights reserved.
+//
+
+import UIKit
+
+class AppButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.layer.borderColor = UIColor.lightGray.cgColor
+        self.layer.borderWidth = 1.0
+        
+    }
+
+
+}
